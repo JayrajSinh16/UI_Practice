4c27f5cf23a507e7a34ca763d505bef714326e85
Jayraj <jayrajsinhzala1024@gmail.com>
2023-10-16 20:10:17 +0530

    Created Category Screen And Radial Painter

--- budget_app_ui/lib/screens/home_screen.dart
+++ budget_app_ui/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:budget_app_ui/data/data.dart';
 import 'package:budget_app_ui/helpers/color_helper.dart';
 import 'package:budget_app_ui/models/category_model.dart';
 import 'package:budget_app_ui/models/expense_model.dart';
+import 'package:budget_app_ui/screens/category_screen.dart';
 import 'package:budget_app_ui/widgets/bar_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -14,90 +15,93 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   _buildCategory(Category category, double totalAmountSpent) {
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      height: 100.0,
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(
-        horizontal: 20.0,
-        vertical: 10.0,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 2),
-            blurRadius: 6.0,
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                category.name,
-                style: const TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Text(
-                "₹ ${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / ${category.maxAmount.toStringAsFixed(2)}",
-                style: const TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          LayoutBuilder(
-            builder: (context, constraints) {
-              print(constraints.maxHeight);
-              print(constraints.maxWidth);
-
-              final double maxBarWidth = constraints.maxWidth;
-              final double precent =
-                  (category.maxAmount - totalAmountSpent) / category.maxAmount;
-              double barWidth = precent * maxBarWidth;
-
-              if (barWidth < 0) {
-                barWidth = 0;
-              }
-              return Stack(
-                children: [
-                  Container(
-                    height: 20.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(15.00),
-                    ),
+    return GestureDetector(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => CategoryScreen(category: category),),),
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        height: 100.0,
+        width: double.infinity,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 10.0,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  category.name,
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
                   ),
-                  Container(
-                    height: 20.0,
-                    width: barWidth,
-                    decoration: BoxDecoration(
-                      color: getColor(
-                        context,
-                        precent,
-                      ),
-                      borderRadius: BorderRadius.circular(
-                        15.00,
+                ),
+                Text(
+                  "₹ ${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / ${category.maxAmount.toStringAsFixed(2)}",
+                  style: const TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                print(constraints.maxHeight);
+                print(constraints.maxWidth);
+    
+                final double maxBarWidth = constraints.maxWidth;
+                final double precent =
+                    (category.maxAmount - totalAmountSpent) / category.maxAmount;
+                double barWidth = precent * maxBarWidth;
+    
+                if (barWidth < 0) {
+                  barWidth = 0;
+                }
+                return Stack(
+                  children: [
+                    Container(
+                      height: 20.0,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15.00),
                       ),
                     ),
-                  )
-                ],
-              );
-            },
-          )
-        ],
+                    Container(
+                      height: 20.0,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                        color: getColor(
+                          context,
+                          precent,
+                        ),
+                        borderRadius: BorderRadius.circular(
+                          15.00,
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
