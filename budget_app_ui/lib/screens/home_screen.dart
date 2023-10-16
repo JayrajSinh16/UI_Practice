67b8b9c28053e62010c0dc93c09a4d848d52ab26
Jayraj <jayrajsinhzala1024@gmail.com>
2023-10-16 18:34:56 +0530

    Created HomeScreen and Bar Chart

---
+++ budget_app_ui/lib/screens/home_screen.dart
@@ -0,0 +1,64 @@
+import 'package:budget_app_ui/data/data.dart';
+import 'package:budget_app_ui/widgets/bar_chart.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text(
+              "Budget Application",
+            ),
+            expandedHeight: 100.0,
+            forceElevated: true,
+            floating: true,
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.settings),
+              iconSize: 30.0,
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                iconSize: 30.0,
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Container(
+                  height: 100,
+                  margin: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],),
+                      child: BarChart(expensses: weeklySpending),
+                );
+              },
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ budget_app_ui/lib/widgets/bar_chart.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BarChart extends StatelessWidget {
+  final List<double> expensses;
+  const BarChart({super.key, required this.expensses});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
